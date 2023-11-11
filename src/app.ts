1c92a38b2bb7d8bb9a0156c8d87d1ae8175edd0e
javier-wq <javiermonge247@gmail.com>
2023-11-11 17:50:48 -0600

    Installing cloudinary to upload images

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import fileUploud from "express-fileupload";
+import fileUpload from "express-fileupload";
 import { router } from "./routes";
 import db from "./config/mongo";
 
@@ -11,6 +11,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./uploads",
+  })
+);
 
 db().then(() => console.log("Connection established successfully"));
 
